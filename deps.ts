80b773d10ce436703a4efdec4e2b9bd8f5188bd5
dk <2597375+Thesephi@users.noreply.github.com>
2024-05-27 02:35:35 +0200

    v0.3.0 - see CHANGELOG for details

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
-export { join } from "jsr:@std/path@^0.218.2";
+export { join } from "jsr:@std/path@^0.225.1";
 
 export {
   Application,
   Context,
   RouteContext,
   Router,
-} from "jsr:@oak/oak@^14.2.0";
+} from "jsr:@oak/oak@^16.0.0";
 
 export type {
   Next,
   RouteParams,
   RouterMiddleware,
   State,
-} from "jsr:@oak/oak@^14.2.0";
+} from "jsr:@oak/oak@^16.0.0";
