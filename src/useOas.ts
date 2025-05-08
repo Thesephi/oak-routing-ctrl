a58fb5c7f0aa182297b649273baf59bd280cd76e
dk <2597375+Thesephi@users.noreply.github.com>
2025-05-08 14:44:38 +0200

    v0.15.0 - see CHANGELOG for details (#48)

--- src/useOas.ts
+++ src/useOas.ts
@@ -74,7 +74,7 @@ const _useOas: UseOas = (
 
   const generator = new OpenApiGenerator(registry.definitions);
   const apiDoc = generator.generateDocument({
-    openapi: "3.0.0",
+    openapi: oasCfg.openapi || "3.0.0",
     info: {
       version: "1.0.0",
       title: "My API",
